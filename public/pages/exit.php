169061afe2e333183b367812cfa77671cae02124
Vladyslav <vladislavshvetsua@gmail.com>
2023-01-14 23:42:06 +0100

    Added exit button functionality

--- public/pages/exit.php
+++ public/pages/exit.php
@@ -1,4 +1,17 @@
 <div>
-    <h2>Вихід</h2>
-    <h4>Контент сторінки виходу</h4>
+    <?php
+    if (isset($_SESSION['user'])) {
+        session_destroy();
+        echo '
+        <h5 style="color: green">
+        You were successfully logged out!
+        </h5>';
+    }else{
+        echo '
+        <h5>
+        You are already logged out!
+        </h5>';
+    }
+
+    ?>
 </div>
